edf04f7e32a8768dee8a3d2708f32767d9f834cc
Администратор <maksimdemsky@gmail.com>
2021-04-28 00:21:28 +0300

    added 50 tests

---
+++ src/test/java/DisabledTests.java
@@ -0,0 +1,87 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DisabledTests {
+    @Test
+    @Disabled
+    void someTest1() {
+        assertTrue(true);
+    }
+    @Test
+    @Disabled
+    void someTest2() {
+        assertTrue(true);
+    }
+    @Test
+    @Disabled
+    void someTest3() {
+        assertTrue(true);
+    }
+    @Test
+    @Disabled
+    void someTest4() {
+        assertTrue(true);
+    }
+    @Test
+    @Disabled
+    void someTest5() {
+        assertTrue(true);
+    }
+    @Test
+    @Disabled
+    void someTest6() {
+        assertTrue(true);
+    }
+    @Test
+    @Disabled
+    void someTest7() {
+        assertTrue(true);
+    }
+    @Test
+    @Disabled
+    void someTest8() {
+        assertTrue(true);
+    }
+    @Test
+    @Disabled
+    void someTest9() {
+        assertTrue(true);
+    }
+    @Test
+    @Disabled
+    void someTest10() {
+        assertTrue(true);
+    }
+    @Test
+    @Disabled
+    void someTest11() {
+        assertTrue(true);
+    }
+    @Test
+    @Disabled
+    void someTest12() {
+        assertTrue(true);
+    }
+    @Test
+    @Disabled
+    void someTest13() {
+        assertTrue(true);
+    }
+    @Test
+    @Disabled
+    void someTest14() {
+        assertTrue(true);
+    }
+    @Test
+    @Disabled
+    void someTest15() {
+        assertTrue(true);
+    }
+    @Test
+    @Disabled
+    void someTest16() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/NegativeTests.java
@@ -0,0 +1,70 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTests {
+    @Test
+    void someTest1() {
+        assertTrue(false);
+    }
+    @Test
+    void someTest2() {
+        assertTrue(false);
+    }
+    @Test
+    void someTest3() {
+        assertTrue(false);
+    }
+    @Test
+    void someTest4() {
+        assertTrue(false);
+    }
+    @Test
+    void someTest5() {
+        assertTrue(false);
+    }
+    @Test
+    void someTest6() {
+        assertTrue(false);
+    }
+    @Test
+    void someTest7() {
+        assertTrue(false);
+    }
+    @Test
+    void someTest8() {
+        assertTrue(false);
+    }
+    @Test
+    void someTest9() {
+        assertTrue(false);
+    }
+    @Test
+    void someTest10() {
+        assertTrue(false);
+    }
+    @Test
+    void someTest11() {
+        assertTrue(false);
+    }
+    @Test
+    void someTest12() {
+        assertTrue(false);
+    }
+    @Test
+    void someTest13() {
+        assertTrue(false);
+    }
+    @Test
+    void someTest14() {
+        assertTrue(false);
+    }
+    @Test
+    void someTest15() {
+        assertTrue(false);
+    }
+    @Test
+    void someTest16() {
+        assertTrue(false);
+    }
+}
